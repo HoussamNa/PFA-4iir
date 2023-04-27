fa29c0e596fd1eda319ec672da615480f6032646
Houssam Nasir <77208956+BabaY4ga-Unow@users.noreply.github.com>
2023-04-27 20:19:25 +0000

    week 5 commit

--- sl/src/App.js
+++ sl/src/App.js
@@ -1,57 +1,49 @@
-import logo from './resources/logo.svg';
-import './static/App.css';
-import './Navber';
+import { useState } from 'react';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import Navbar from './Navber';
 import Inscription from './Inscrir';
 import HomeVue from './LandingPage';
 import About from './about';
-import { useState } from 'react';
-import { BrowserRouter, Switch, Route, Link, Routes } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import MainContent from './MainPage';
 import ScanQrCode from './QrReader';
-import MapContainer from './Map';
+
 function App() {
   const [showInscription, setShowInscription] = useState(false);
+  const [content, setContent] = useState('default');
+  const [currentChoice, setCurrentChoice] = useState('option1');
 
-  const toggleInscription = () => {
+  const toggleInscription = () => setShowInscription(!showInscription);
+
+  const handleClick = () => {
+    setContent('nouveau contenu');
     setShowInscription(!showInscription);
   };
-  const [content, setContent] = useState('default'); // le contenu initial est "default"
-  
-  const handleClick = () => {
-    setContent('nouveau contenu'); // changer le contenu en cliquant sur le bouton
-    setShowInscription(!showInscription);
-  }
 
-  const [currentChoice, setCurrentChoice] = useState("option1");
+  const handleChoiceSelect = (choice) => setCurrentChoice(choice);
 
-  function handleChoiceSelect(choice) {
-    setCurrentChoice(choice);
-  }
   return (
     <div className="app-container">
       <div className="landing-page-container">
-      {showInscription && (
-        <div className="popup-container">
-          <Inscription  onConnect={handleClick} />
-        </div>
-      )}
-      {content === 'default' ? (
-        <div>
-        <Navbar />
-        <HomeVue onSignUpClick={toggleInscription} />
-        <About />
-        <ScanQrCode/>
-       
-        </div>
-      ) : (
-        <div>
-          <Sidebar  onChoiceSelect={handleChoiceSelect}/>
-          <MainContent  currentChoice={currentChoice}/>
-        </div>
-      )}
-   </div>    
+        {showInscription && (
+          <div className="popup-container">
+            <Inscription onConnect={handleClick} />
+          </div>
+        )}
+        {content === 'default' ? (
+          <div>
+            <Navbar />
+            <HomeVue onSignUpClick={toggleInscription} />
+            <About />
+            <ScanQrCode />
+          </div>
+        ) : (
+          <div>
+            <Sidebar onChoiceSelect={handleChoiceSelect} />
+            <MainContent currentChoice={currentChoice} />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
